a0b46ad94889ab6c8600c1ff9bcd30384d0542cd
CR <rajshekhar@protonmail.com>
2020-06-25 14:33:39 -0600

    only plus + operator strings

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -6,13 +6,11 @@ let currentResult = defaultResult;
 
 function add() {
     //here currentResul treated as number and  userInput as string..so to covert this to number we have inbuilt methods like parseInt, parseFloat
-// aslo we can use , if we want non decimal number currentResult = currentResult
-// + +(userInput.value);
+// aslo we can use , if we want non decimal number currentResult = currentResult + +(userInput.value); // only '+' operator supports strings
     currentResult = currentResult + parseInt(userInput.value);
 
     /* convert number to string, as we know below currentResult is storing numbers only so to convert this ...EX: userInput =10 and result would be like 1010 */
 currentResult = currentResult.toString() + parseInt(userInput.value);
-
     outputResult(currentResult, '');
 }
 
