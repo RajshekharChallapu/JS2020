9a6d9d5b76b2974b1dde81ffb8cfddd7d2388c17
CR <rajshekhar@protonmail.com>
2020-06-24 20:09:14 -0600

    assignment new file created

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,7 +2,7 @@
 // const used for fixed values
 const defaultResult =0;
 
-//assigning const variable --value is always '0'
+//assigning const vaiable --value is always '0'
 let currentResult = defaultResult;
 
 currentResult = (currentResult + 10) * 3/2-1;
@@ -10,7 +10,6 @@ currentResult = (currentResult + 10) * 3/2-1;
 //enclosed by backtick is called template literal and "$" sign with {currly} braces are placeholders 
 let test= `( ${defaultResult} + 10) * 3/2-1`;
 
-let errorMessage = 'An error' + ' occurred!';
 
-// print the results by calling decalred params
+// print the results by calling decalred param
 outputResult(currentResult, errorMessage);
---
+++ basics-01-starting-project/assets/scripts/assignment.js
@@ -0,0 +1,24 @@
+//we used const cuz userInput value never chnaged 
+const userInput= 20;
+
+// result variable declared here
+let result;
+
+// variable result values assigned here with diff operators
+result = 18 + userInput; //38
+
+//step1: 38 -10 = 28
+result = result -10; 
+
+//Step2: step1 o/p value stored in result nad 28 * 2 = 56 
+result = result*2; 
+
+//Step3: ste2 o/p value stored i result variable and 56 /4 = 14
+result = result/4; // 14
+
+
+// prints variableuserInput value
+alert(userInput);
+
+//final step 3 value prints...is going to store in let result variable;
+alert(result);
