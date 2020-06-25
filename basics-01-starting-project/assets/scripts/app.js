cae16f726c902075de2509d5518d6c0b1cfd9285
CR <rajshekhar@protonmail.com>
2020-06-25 14:46:14 -0600

    reusable variable

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -3,11 +3,14 @@ const defaultResult = 10;
 let currentResult = defaultResult;
 
 
+
 function add() {
+      //resuable variable and it is converting string to number
+    const enteredNumber = parseInt(usrInput.value)
     // any vaule passded in to this literal is coverted as string by default
-const calcDescription = `${currentResult} + ${userInput.value}`
-    currentResult = currentResult + parseInt(userInput.value);
-    outputResult(currentResult,calcDescription );
+    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult + parseInt(enteredNumber);
+    outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener('click', add);
