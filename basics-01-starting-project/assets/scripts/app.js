5d1fd684777e57f087b301d008a0ea2c4f1f0c73
CR <rajshekhar@protonmail.com>
2020-06-24 21:15:14 -0600

    variable access

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -3,14 +3,18 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+/* we can use functions anywhere in the file..but good to have functions in above the file*/
+/* this function can have access to global variables that declared above ex: const and let */
+/* pure functions */
+function add(a, b) {
+    /* this is called local or block scope..means availble to only this function */
+    const result = a + b;
+    return result;
+}
+
  currentResult = add (1,2);
 
 let test= `( ${defaultResult} + 10) * 3/2-1`;
 
 outputResult(currentResult, test);
 
-/* we can use functions anywhere in the file..but good to have functions in above the file */
-function add(a, b) {
-    const result = a + b;
-    return result;
-}
