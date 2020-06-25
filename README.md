f89afa8d09df10495ced8a2db9dc0827df8174b2
CR <rajshekhar@protonmail.com>
2020-06-24 19:00:13 -0600

    initial commit of basic variables

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # JS2020
+
+
+# githu solutions ==== "https://www.git-tower.com/learn/git/faq/create-branch"
+
+#  
---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,12 @@
+
+// const used for fixed values
+const defaultResult =0;
+
+//assigning const vaiable --value is always '0'
+let currentResult = defaultResult;
+
+currentResult = (currentResult + 10) * 3/2-1;
+
+let test= '(' + defaultResult + '+ 10) * 3/2-1';
+
+outputResult(currentResult, test);
