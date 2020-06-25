9c93a1de42277129837b98d23489c49927cf00d8
CR <rajshekhar@protonmail.com>
2020-06-24 19:43:27 -0600

    printing different param results

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -7,6 +7,10 @@ let currentResult = defaultResult;
 
 currentResult = (currentResult + 10) * 3/2-1;
 
-let test= '(' + defaultResult + '+ 10) * 3/2-1';
+//enclosed by backtick is called template literal and "$" sign with {currly} braces are placeholders 
+let test= `( ${defaultResult} + 10) * 3/2-1`;
 
-outputResult(currentResult, test);
+let errorMessage = 'An error' + ' occurred!';
+
+// print the results by calling decalred params
+outputResult(currentResult, errorMessage);
