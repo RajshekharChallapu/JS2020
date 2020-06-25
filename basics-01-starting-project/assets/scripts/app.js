f10f404849ba1ae01790f9520554b3ace246c028
CR <rajshekhar@protonmail.com>
2020-06-25 15:17:23 -0600

    multiple functions calling

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,21 +1,50 @@
-
 const defaultResult = 10;
 let currentResult = defaultResult;
 
-/* creating a num function to get splitting code into functions and calling usefull for more code scenarios */
-function getUserInputNumber(){
-return parseInt(usrInput.value);
+function getUserInputNumber() {
+    return parseInt(usrInput.value);
+}
+
+/* called by add function and takes params data from that func and stroing into new variable and printing o/p result */
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+
 }
 
 function add() {
+    /* stores the function value and calling function..getUserInputNumber func do the coversion and returns the value */
     const enteredNumber = getUserInputNumber();
-    const calcDescription = `${currentResult} + ${enteredNumber}`;
-    currentResult = currentResult +enteredNumber;
-    outputResult(currentResult, calcDescription);
+    /* storing currentresult value */
+    const initialResult = currentResult;
+    /* calculation of current value and user entered number*/
+    currentResult = currentResult + enteredNumber;
+    /* calling new function with params within the function  */
+    createAndWriteOutput('+', initialResult, enteredNumber);
 }
 
+function subtract() {
+    const enteredNumber = getUserInputNumber();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserInputNumber();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
+}
+function divide() {
+    const enteredNumber = getUserInputNumber();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);
+}
+
+/* listners and calling functions */
 addBtn.addEventListener('click', add);
-
-
-
-
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
