26491ab490c6c04218e6ea734b0afc117ebfeb44
CR <rajshekhar@protonmail.com>
2020-06-25 14:39:51 -0600

    using literals

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -3,15 +3,11 @@ const defaultResult = 10;
 let currentResult = defaultResult;
 
 
-
 function add() {
-    //here currentResul treated as number and  userInput as string..so to covert this to number we have inbuilt methods like parseInt, parseFloat
-// aslo we can use , if we want non decimal number currentResult = currentResult + +(userInput.value); // only '+' operator supports strings
+    // any vaule passded in to this literal is coverted as string by default
+const calcDescription = `${currentResult} + ${userInput.value}`
     currentResult = currentResult + parseInt(userInput.value);
-
-    /* convert number to string, as we know below currentResult is storing numbers only so to convert this ...EX: userInput =10 and result would be like 1010 */
-currentResult = currentResult.toString() + parseInt(userInput.value);
-    outputResult(currentResult, '');
+    outputResult(currentResult,calcDescription );
 }
 
 addBtn.addEventListener('click', add);
