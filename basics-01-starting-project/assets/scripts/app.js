cac19443f851fb2af64350808e9a696ecc29183e
CR <rajshekhar@protonmail.com>
2020-06-25 23:52:18 -0600

    reusable function and building dynamic obj

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -11,24 +11,27 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
     outputResult(currentResult, calcDescription);
 }
 
+/* takes input from add function that added as params in writeToLog funct */
+function writeToLog(operationIdentifier, prevResult, operationNumber, newResult ){
+    /* getting dynamic data from writeToLog function params and builds objects */
+const logEntry = {
+    operation: operationIdentifier,
+    preResult: prevResult,
+    number: operationNumber,
+    result: newResult
+};
+logEntries.push(logEntry);
+console.log(logEntries);
+    
+}
+
 function add() {
     const enteredNumber = getUserInputNumber();
     const initialResult = currentResult;
     currentResult = currentResult + enteredNumber; 
     createAndWriteOutput('+', initialResult, enteredNumber);
-    /* object creation and using onjetc in function by function variables */
-    const logEntry = {
-        operation: 'ADD',
-        preResult: initialResult,
-        number: enteredNumber,
-        result: currentResult
-    };
-     logEntries.push(logEntry);
-    /* adding values to arry list */
-    logEntries.push(enteredNumber);
-    /* "." notation allows to access object parameters */
-    console.log(logEntry.operation);
-    console.log(logEntries);
+    /* reusable function and passing data to write to log params..here these param data is formed after all the above calculations*/
+    writeToLog('ADD', initialResult,enteredNumber, currentResult);
 }
 
 function subtract() {
@@ -36,6 +39,8 @@ function subtract() {
     const initialResult = currentResult;
     currentResult -= enteredNumber;
     createAndWriteOutput('-', initialResult, enteredNumber);
+    writeToLog('Subtract', initialResult, enteredNumber, currentResult);
+
 }
 
 function multiply() {
@@ -43,12 +48,14 @@ function multiply() {
     const initialResult = currentResult;
     currentResult *= enteredNumber;
     createAndWriteOutput('*', initialResult, enteredNumber);
+    writeToLog('Multiply', initialResult, enteredNumber, currentResult);
 }
 function divide() {
     const enteredNumber = getUserInputNumber();
     const initialResult = currentResult;
     currentResult /= enteredNumber;
     createAndWriteOutput('/', initialResult, enteredNumber);
+    writeToLog('Divide', initialResult, enteredNumber, currentResult);
 }
 
 /* listners and calling functions */
