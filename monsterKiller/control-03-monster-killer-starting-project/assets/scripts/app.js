801f1bd0bb534baa7abadb6a532df2697ccac5f0
CR <rajshekhar@protonmail.com>
2020-07-04 19:37:59 -0600

    for loop usage for logs

--- monsterKiller/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ monsterKiller/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -193,6 +193,14 @@ function healPlayerHandler() {
 }
 
 function printLogHandler() {
+    for (let i= 0; i < 3; i++){
+        console.log('=====');
+    }
+// for (let i = 10; i > 0; i--) {
+        
+//         console.log(i);
+
+//     }
     console.log(battleLog);
 }
 
