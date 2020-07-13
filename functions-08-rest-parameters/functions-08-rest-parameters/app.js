0ffca689c924b14f39f7450ec26a7360811e417b
Raja Challapu <rajchallapu@outlook.com>
2020-07-13 14:02:06 -0600

    function inside a function

--- functions-08-rest-parameters/functions-08-rest-parameters/app.js
+++ functions-08-rest-parameters/functions-08-rest-parameters/app.js
@@ -84,9 +84,15 @@ startGameBtn.addEventListener('click', () => {
 // not related to game
 
 const sumUp = (a, b, ...numbers) => {
+ // creating a function inside function
+ const validateNumber = (number) => {
+   return isNaN(number) ? 0 : number;
+
+ };
+
   let sum = 0;
   for (const num of numbers) {
-    sum += num;
+    sum += validateNumber(num);
   }
   return sum;
 };
@@ -103,6 +109,6 @@ const subtractUp = function(numbers){
   return sum;
 };
 
-console.log(sumUp(1, 5, 10, -3, 6, 10));
+console.log(sumUp(1, 'dgjdgj', 10, -3, 6, 10));
 console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
 console.log(subtractUp(1, 10, 15, 20));
