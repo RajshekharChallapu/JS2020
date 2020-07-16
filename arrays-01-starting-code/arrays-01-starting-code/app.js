47af46665378de57d733087111abfa3636a2af28
Raja Challapu <rajchallapu@outlook.com>
2020-07-15 23:02:44 -0600

    concat array

--- arrays-01-starting-code/arrays-01-starting-code/app.js
+++ arrays-01-starting-code/arrays-01-starting-code/app.js
@@ -46,20 +46,30 @@
 // console.log(personalData[1]);
 
 // push and pop are good to use
-const hobbies = ['Sports','cook'];
-//adding elements to array...this adds elements at the end
-hobbies.push('no cook');
-// adding  element at the begning unshift() method
-hobbies.unshift('reading');
+// const hobbies = ['Sports','cook'];
+// //adding elements to array...this adds elements at the end
+// hobbies.push('no cook');
+// // adding  element at the begning unshift() method
+// hobbies.unshift('reading');
 
-// pop() removes end elements.. beloow will help to store the value in variable
-const poppedValue = hobbies.pop();
-hobbies.shift();
-console.log(hobbies);
+// // pop() removes end elements.. beloow will help to store the value in variable
+// const poppedValue = hobbies.pop();
+// hobbies.shift();
+// console.log(hobbies);
 
-hobbies[1] ='reading';
-hobbies[5] ='cook';
-console.log(hobbies);
+// hobbies[1] ='reading';
+// hobbies[5] ='cook';
+// console.log(hobbies);
 
-const removableElements = hobbies.splice(-1, 1);
-console.log(hobbies);
+// const removableElements = hobbies.splice(-1, 1);
+// console.log(hobbies);
+
+
+const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+//const storeResults =testResults.slice(2);
+//takes one or more array..creates a copy of array and creates new array
+const storeResults = testResults.concat([4.55,4]);
+
+
+testResults.push(15.65);
+console.log(storeResults, testResults);
