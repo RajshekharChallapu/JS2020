cee7ec77a5b436a462d9f0174a6b2c6477823d33
Raja Challapu <rajchallapu@outlook.com>
2020-07-19 23:07:21 -0600

    rendering elements based on objects

--- objects01/objects-01-starting-project/assets/scripts/objects.js
+++ objects01/objects-01-starting-project/assets/scripts/objects.js
@@ -4,6 +4,24 @@ const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
+const renderMovies = () => {
+    const movieList = document.getElementById('movie-list');
+
+    if(movies.length === 0){
+        movieList.classList.remove('visible');
+        return;
+    } else {
+        movieList.classList.add('visible');
+    }
+movieList.innerHTML = '';
+
+movies.forEach((movie) => {
+    const movieEl = document.createElement('li');
+    movieEl.textContent = movie.info.title;
+    movieList.append(movieEl);
+});
+};
+
 const addMovieHandler = () =>{
     const title = document.getElementById('title').value;
     const extraName = document.getElementById('extra-name').value;
@@ -24,7 +42,7 @@ const addMovieHandler = () =>{
 id: Math.random()
 };
 movies.push(newMovie);
-console.log(newMovie);
+renderMovies();
 };
 
 addMovieBtn.addEventListener('click', addMovieHandler);
