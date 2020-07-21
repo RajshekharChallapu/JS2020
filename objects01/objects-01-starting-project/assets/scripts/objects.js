66496331d47e8bc1d79fdc8bc4d6aec03c4e5b16
Raja Challapu <rajchallapu@outlook.com>
2020-07-20 21:52:45 -0600

    object destructuring

--- objects01/objects-01-starting-project/assets/scripts/objects.js
+++ objects01/objects-01-starting-project/assets/scripts/objects.js
@@ -21,10 +21,13 @@ const filteredMovies = !filter
 
 filteredMovies.forEach((movie) => {
     const movieEl = document.createElement('li');
-    let text = movie.info.title + ' - ';
-    for (const key in movie.info) {
+    const { info, ...otherProps } = movie; 
+    console.log(otherProps);
+    const { title : movieTitle } = info;
+    let text = title + ' - ';
+    for (const key in info) {
         if(key !== 'title'){
-            text = text + `${key}: ${movie.info[key]}`;
+            text = text + `${key}: ${info[key]}`;
         }
     }
     movieEl.textContent = text;
@@ -49,7 +52,7 @@ const addMovieHandler = () =>{
             [extraName]: extraValue
     },
 
-id: Math.random()
+id: Math.random().toString()
 };
 movies.push(newMovie);
 renderMovies();
